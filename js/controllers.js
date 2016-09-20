d9e7405fde9a312fb7ff436cc5245fa9eb3872b4
nilsonreguan <nilsonreguan@gmail.com>
2016-09-20 05:03:18 -0600

    6
    6

--- js/controllers.js
+++ js/controllers.js
@@ -55,4 +55,71 @@ function ($scope, $stateParams) {
 
 
 }])
+
+
+.controller('MyCtrl', function($scope, $timeout) {
+  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+  
+  $scope.doRefresh = function() {
+    
+    console.log('Refreshing!');
+    $timeout( function() {
+      //simulate async response
+      $scope.items.push('New Item ' + Math.floor(Math.random() * 1000) + 4);
+
+      //Stop the ion-refresher from spinning
+      $scope.$broadcast('scroll.refreshComplete');
+    
+    }, 1000);
+      
+  };
+  
+});
+
+module.run(function($cordovaPush) {
+
+  var androidConfig = {
+    "senderID": "685314917852",
+  };
+
+  document.addEventListener("deviceready", function(){
+    $cordovaPush.register(androidConfig).then(function(result) {
+      // Success
+    }, function(err) {
+      // Error
+    })
+
+    $rootScope.$on('$cordovaPush:notificationReceived', function(event, notification) {
+      switch(notification.event) {
+        case 'registered':
+          if (notification.regid.length > 0 ) {
+            alert('registration ID = ' + notification.regid);
+          }
+          break;
+
+        case 'message':
+          // this is the actual push notification. its format depends on the data model from the push server
+          alert('message = ' + notification.message + ' msgCount = ' + notification.msgcnt);
+          break;
+
+        case 'error':
+          alert('GCM error = ' + notification.msg);
+          break;
+
+        default:
+          alert('An unknown GCM event has occurred');
+          break;
+      }
+    });
+
+
+    // WARNING: dangerous to unregister (results in loss of tokenID)
+    $cordovaPush.unregister(options).then(function(result) {
+      // Success!
+    }, function(err) {
+      // Error
+    })
+
+  }, false);
+});
  
