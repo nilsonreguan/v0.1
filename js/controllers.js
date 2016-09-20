ae212f54a90c470574a7ff8bfa737507a91c9967
nilsonreguan <nilsonreguan@gmail.com>
2016-09-20 03:37:59 -0600

    5
    5

---
+++ js/controllers.js
@@ -0,0 +1,58 @@
+angular.module('app.controllers', [])
+  
+.controller('verCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('buscarCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('interesCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+      
+.controller('appDemoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('ingresarCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('sugerenciasCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('calificacionesCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ js/routes.js
@@ -0,0 +1,81 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController.ver', {
+    url: '/page2',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/ver.html',
+        controller: 'verCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.buscar', {
+    url: '/page3',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/buscar.html',
+        controller: 'buscarCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.interes', {
+    url: '/page4',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/interes.html',
+        controller: 'interesCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('appDemo', {
+    url: '/page5',
+    templateUrl: 'templates/appDemo.html',
+    controller: 'appDemoCtrl'
+  })
+
+  .state('ingresar', {
+    url: '/page6',
+    templateUrl: 'templates/ingresar.html',
+    controller: 'ingresarCtrl'
+  })
+
+  .state('tabsController.sugerencias', {
+    url: '/page7',
+    views: {
+      'tab6': {
+        templateUrl: 'templates/sugerencias.html',
+        controller: 'sugerenciasCtrl'
+      }
+    }
+  })
+
+  .state('calificaciones', {
+    url: '/page8',
+    templateUrl: 'templates/calificaciones.html',
+    controller: 'calificacionesCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page5')
+
+  
+
+});
